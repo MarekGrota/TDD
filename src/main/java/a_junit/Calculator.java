e8d39fe9aa094c4ff6f9fb5d6a8f8a9cf1053b95
Marek Grota <marekgro@gmail.com>
2020-08-23 09:08:54 +0200

    First commit

--- src/main/java/a_junit/Calculator.java
+++ src/main/java/a_junit/Calculator.java
@@ -1,7 +1,22 @@
 package a_junit;
 
 public class Calculator {
+
     public Integer add(Integer a, Integer b) {
         return a + b;
     }
+
+    public Integer subtraction(Integer a, Integer b) {
+        return a - b;
+    }
+
+    public Integer multiplication(Integer a, Integer b) {
+        return a * b;
+    }
+    public Integer division(Integer a, Integer b) {
+        return a / b;
+    }
+
+
+
 }
--- src/test/java/a_junit/CalculatorTest.java
+++ src/test/java/a_junit/CalculatorTest.java
@@ -1,20 +1,72 @@
 package a_junit;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class CalculatorTest {
 
-    @Test
-    public void test() {
+    Calculator calculator;
 
-        // Arrange
-        Calculator calculator = new Calculator();
+    @BeforeAll
+    private static void beforeAll() {
+
+        System.out.println("BEFORE ALL.");
+    }
+
+    @AfterAll
+    private static void afterAll() {
+
+        System.out.println("AFTER ALL.");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("BEFORE EACH.");
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("AFTER EACH.");
+    }
+
+    @Test
+    //@Disabled
+    public void add_2Plus2_shouldBeEqualTo4() {
 
         // Act
         Integer sum = calculator.add(2,2);
 
         // Assert
         Assertions.assertEquals(4, sum);
+    }
+
+    @Test
+    public void subtraction_2Minus2_shouldBeEqualTo0() {
+
+        // Act
+        Integer sub = calculator.subtraction(2,2);
+
+        // Assert
+        Assertions.assertEquals(0, sub);
+    }
+
+    @Test
+    public void multiplication_2times2_shouldBeEqualTo4() {
+
+        // Act
+        Integer multi = calculator.multiplication(2,2);
+
+        // Assert
+        Assertions.assertEquals(4, multi);
+    }
+
+    @Test
+    public void division_2divide2_shouldBeEqualTo1() {
+
+        // Act
+        Integer div = calculator.division(2,2);
+
+        // Assert
+        Assertions.assertEquals(1, div);
     }
 }
