8b3157b4f484d0f6ef7cd094aebd4fd7f618bd77
Marek Grota <marekgro@gmail.com>
2020-08-26 18:19:48 +0200

    add LeapYearCheckerTest TDD example

--- src/main/java/g_tdd/LeapYearChecker.java
+++ src/main/java/g_tdd/LeapYearChecker.java
@@ -5,7 +5,8 @@ public class LeapYearChecker {
    public boolean isLeapYear(int year){
        if ((year % 4 == 0) && (year % 100 != 0)) {
            return true;
-       } else if (year % 400 == 0)
+       }
+       if (year % 400 == 0)
            return true;
        return false;
    }
